c2af40df52d8984f92caef0ba5d75f3783e8d9f2
Alan <403466681@qq.com>
2017-07-23 23:53:18 +0800

    lesson 8

---
+++ lesson 8.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+with tf.device('cpu:0'):
+    state = tf.Variable(0,name='counter')
+
+    #print(state.name)
+    one = tf.constant(1)
+
+    new_value = tf.add(state, one)
+    update = tf.assign(state,new_value)
+    init = tf.global_variables_initializer()  #如果有变量，一定要写这一句，初始化所有变量
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for _ in range(3):
+            sess.run(update)
+            print(sess.run(state))
